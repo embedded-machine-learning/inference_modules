421e620eb5a33fa44705b6661f8df3d9533ceaff
maivnov <matvey.ivanov@gmx.at>
2022-02-23 09:43:25 +0000

    added RPi4 parser skeleton

---
+++ hw_modules/rpi4/parser.py
@@ -0,0 +1,24 @@
+# The PyARMNN profiler delivers per layer statistics, but is not used due to the black box approach
+
+__author__ = "Matvey Ivanov"
+__copyright__ = "Christian Doppler Laboratory for Embedded Machine Learning"
+__license__ = "Apache 2.0"
+
+def add_measured_to_input(time_df, input_df, measured_df):
+    return True
+
+def read_report(report="", outfolder="./tmp/", format=None):
+    return True
+
+def r2a(report):
+    return True
+
+def extract_data_from_folder(infold, outfold):
+    return True
+
+def main():
+    print("RPi4 Parser")
+    return True
+
+if __name__ == "__main__":
+    main()
