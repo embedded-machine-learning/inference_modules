6504391beac936a00f4b0166bb6cb306ffb84d16
maivnov <matvey.ivanov@gmx.at>
2022-02-24 16:25:54 +0000

    changed paths to absolute from remote execution

--- tests/test_rpi4.py
+++ tests/test_rpi4.py
@@ -12,14 +12,14 @@ __author__ = "Matthias Wess, Matvey Ivanov"
 __copyright__ = "Christian Doppler Laboratory for Embedded Machine Learning"
 __license__ = "Apache 2.0"
 
-def test_optimize_network(network="mobilenetv2-7-sim0.tflite"):
-    test_net = os.path.join('tests', 'networks', network)
+def test_optimize_network(network="/home/ubuntu/imatvey/inference_modules/tests/networks/mobilenetv2-7-sim0.tflite"):
+    test_net = network
     rpi4.inference.optimize_network(model_path=test_net, network = "tmp_net", input_shape = [1, 224, 224, 3],
                                     input_node = "data", save_folder = "./tmp")
     assert True
 
-def test_run_network(network="mobilenetv2-7-sim0.tflite"):
-    test_net = os.path.join('tests', 'networks', network)
+def test_run_network(network="/home/ubuntu/imatvey/inference_modules/tests/networks/mobilenetv2-7-sim0.tflite"):
+    test_net = network
     # test TFLite Interpreter
     rpi4.inference.run_network(tflite_path = test_net, save_dir = "./tmp", niter = 10, print_bool = True, sleep_time=0,
                 use_tflite=True, use_pyarmnn=False)
@@ -36,8 +36,8 @@ def test_rpi4_r2a(network=None):
     #ncs2.parser.r2a(report_file)
     assert True
 
-def test_all(network="mobilenetv2-7-sim0.tflite"):
-    test_net = os.path.join('tests', 'networks', network)
+def test_all(network="/home/ubuntu/imatvey/inference_modules/tests/networks/mobilenetv2-7-sim0.tflite"):
+    test_net = network
 
     rpi4.inference.optimize_network(model_path=test_net, network = "tmp_net", input_shape = [1, 224, 224, 3],
                                     input_node = "data", save_folder = "./tmp")
@@ -61,7 +61,7 @@ def main():
     test_run_network()
     network = "mobilenetv2-7-sim0"
     test_read_rpi4_report(network=network)
-    test_all(network='mobilenetv2-7-sim0.tflite')
+    test_all(network='/home/ubuntu/imatvey/inference_modules/tests/networks/mobilenetv2-7-sim0.tflite')
 
 if __name__ == '__main__':
     main()
